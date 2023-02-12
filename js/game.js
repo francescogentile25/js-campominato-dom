bf4645f559e18488fc9c805db7c9c9992418af8c
francescogentile25 <francescogentile250893@gmail.com>
2023-02-13 00:59:28 +0100

    FEAT  campo minaro orribile

--- js/game.js
+++ js/game.js
@@ -1,104 +1,72 @@
-console.log('griglia')
-
 const platBtnElement = document.getElementById('play-btn')
 const grigliaElement = document.querySelector('.griglia')
+let score =0
 
-// console.log(platBtnElement)
-platBtnElement.addEventListener('click', startGame) //sen le tonde altrimenti viene invocata subito
-
-// no numeri doppi
-// nuimeri compresi tra 1 3 numeroDiCelle
-const bombe = []
+platBtnElement.addEventListener('click', startGame) //senza  le tonde altrimenti viene invocata subito
+let bombe = []
+console.log(bombe)
 while (bombe.length < 16) {
-    const randomNumber = Math.floor(Math.random() * 64)
-    if (bombe.indexOf(randomNumber) ===  -1) {
+    const randomNumber = Math.floor(Math.random() * (64 - 1 + 1) + 1)
+    if (bombe.indexOf(randomNumber) ===  -1) { // indexOf restituisce l'indice del primo elemento di un array che soddisfa una condizione  restituisce -1 se l'elemento non è presente avrei potuto usare includes
         bombe.push(randomNumber)
     }
 }
-console.log(bombe)
-
-// startGame()
-// bombe.includes('5')
-
-const secondaFunzione = startGame
-const altraVariabile = startGame()
-
-// console.log(secondaFunzione)
-// console.log(altraVariabile)
+// console.log(bombe)
 
 function resetGame() {
-	// azzerare il punteggio
-
+	score =0
 	// svuotare la griglia
 	grigliaElement.innerHTML = ''
-
+	
 	// eliminare eventuali messaggi di game over...
 }
 
 function generaGriglia(latoGriglia) {
 	let numeroCelle = latoGriglia * latoGriglia
-
 	for (let i = 0; i < numeroCelle; i++) {
 		let num = i + 1
 		// console.log(num)
 		let divString = `<div class="cella" style="width: calc(100% / ${latoGriglia});" >${num}</div>` // <div> + num + </div>
-
 		grigliaElement.innerHTML += divString
 	}
 }
 
 function startGame() {
 	console.log('start game')
-
 	// eseguire tutte le operazioni di reset
 	resetGame()
-	let lato = 8
+	let lato = 8 //potrei inserirlo  con un select in num 8 
 	generaGriglia(lato)
-
 	const celleElements = document.querySelectorAll('.cella')
 	// console.log(celleElements)
-
 	for (let i = 0; i < celleElements.length; i++) {
 		const cella = celleElements[i]
-
-		// cella.addEventListener('click', function () {
-		// 	console.log(i + 1)
-		// 	// const cella = ''
-		// 	console.log(cella)
-		// })
-
 		cella.addEventListener('click', onClick)
 	}
-
-	return 'Fine star game'
+	
 }
 
 function onClick(event) {
-	console.log(event)
-	console.log(event.target)
-	console.log(this)
-	console.log(event.target === this)
-	// const cella = event.target
 	const cella = this
-    const numCella = parseInt(cella.innerHTML);
-    console.log(numCella)
-
-    if (bombe.includes(numCella - 1)) {
-        console.log("Hai perso!");
-    }
-
-	cella.classList.add('bg-green')
-	cella.removeEventListener('click', onClick)
-	// console.log(i + 1)
+	const numCella = parseInt(cella.innerHTML);
+	// console.log(numCella)
+	if (bombe.includes(numCella)) {
+		console.log("Hai perso!");
+		cella.classList.add('bg-red')
+		cella.innerHTML= `<i class="fa-solid fa-bomb fs-1"></i>`
+		alert('hai perso')
+		const celleElements = document.querySelectorAll('.cella')
+		for (let i = 0; i < celleElements.length; i++) {
+			celleElements[i].removeEventListener('click', onClick)
+		}
+	}else{
+		cella.classList.add('bg-gray')
+		cella.removeEventListener('click', onClick)
+		score++
+		const punteggio = document.getElementById('score')
+		punteggio.innerHTML = score
+		if (score === 48){
+			alert('hai vinto')
+		}
+	}
 }
-
-// grigliaElement.addEventListener('click', function (event) {
-// 	console.log('target:', event.target)
-// 	console.log('this:', this)
-
-// 	let griglia = this
-// 	let cella = event.target
-// 	cella.classList.add('bg-green')
-// })
-
-
